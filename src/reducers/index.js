9bbc246ea3749fbf4df141fb1fd7f0dc1073f9a4
arulk8 <arulmurugan194@gmail.com>
2018-11-01 08:05:53 +0530

    added Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import PostReducer from './post-reducer';
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostReducer
 });
 
 export default rootReducer;
