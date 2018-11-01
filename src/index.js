d589513da3424af47e13899b59e2fa7c4cb8ed9b
Arulmurugan.k <arulmurugan194@gmail.com>
2018-11-01 08:07:51 +0530

    tested the change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import Promise from 'redux-promise';
 import { BrowserRouter, Route } from 'react-router-dom';
-// import App from './components/app';
+// import App from './components/app'; 
+// we dont need App for now
 import reducers from './reducers';
 import PostIndex from './components/post-index';
 const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
