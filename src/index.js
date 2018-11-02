e63a4abf664d372675da5abf10ed87b6b3dde34d
arulk8 <arulmurugan194@gmail.com>
2018-11-02 08:11:08 +0530

    added routing

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import Promise from 'redux-promise';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import App from './components/app'; 
 // we dont need App for now
 import reducers from './reducers';
 import PostIndex from './components/post-index';
+import PostNew from './components/new-post';
 const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/posts" component={PostIndex} />
+        <Switch>
+
+          <Route path="/posts/new" component={PostNew} />
+          <Route path="/" component={PostIndex} />
+
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
